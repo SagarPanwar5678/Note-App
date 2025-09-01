dcbab0107e3605e4fcb45511eab9c80573eebe73
Sagar Panwar <sagarpanwar5678@gmail.com>
2025-09-01 13:18:03 +0530

    create a api.js

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://localhost:5000", // change if your backend deployed
+});
+
+export default API;
+
+//Is file me gadbad ho sakti hai
