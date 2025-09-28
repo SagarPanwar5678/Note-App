ba758e301546ea5ad31d6ae7434405fade8ba3f7
SagarPanwar5678 <sagarpanwar5678@gmail.com>
2025-09-28 11:40:57 +0530

    Update SignupForm.js

--- frontend/src/components/SignupForm.js
+++ frontend/src/components/SignupForm.js
@@ -72,30 +72,31 @@
 
 import { useState } from "react";
 import API from "../utils/api";
-import "../CSS_folder/SignupForm.css"
-export default function SignupForm({ setUser,setShowSignup }) {
+import "../CSS_folder/SignupForm.css";
+export default function SignupForm({ setUser, setShowSignup, setToken }) {
   const [step, setStep] = useState(1);
   const [formData, setFormData] = useState({
     name: "",
     dateOfBirth: "",
     email: "",
-    otp: ""
+    otp: "",
   });
   const [message, setMessage] = useState("");
 
-  const handleChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+  const handleChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
   const sendOtp = async () => {
     try {
       const res = await API.post("/api/auth/signup/send-otp", {
         name: formData.name,
         dateOfBirth: formData.dateOfBirth,
-        email: formData.email
+        email: formData.email,
       });
+      console.log();
       setMessage(res.data.message);
       setStep(2);
     } catch (err) {
-      console.log(err);
       setMessage(err.response?.data?.message || "Error sending OTP");
     }
   };
@@ -105,11 +106,13 @@ export default function SignupForm({ setUser,setShowSignup }) {
     try {
       const res = await API.post("/api/auth/signup/verify-otp", {
         email: formData.email,
-        otp: formData.otp
+        otp: formData.otp,
       });
       localStorage.setItem("token", res.data.token);
       localStorage.setItem("user", JSON.stringify(res.data.user));
       setUser(res.data.user);
+      setToken(res.data.token);
+      console.log(res.data.message);
     } catch (err) {
       console.log(err);
       setMessage(err.response?.data?.message || "Error verifying OTP");
@@ -118,56 +121,101 @@ export default function SignupForm({ setUser,setShowSignup }) {
 
   return (
     <div>
-  <header className="header">
-    <img src="/icon.png" alt="Logo" className="logo" />
-    <h1>HD</h1>
-  </header>
+      <header className="header">
+        <img src="/icon.png" alt="Logo" className="logo" />
+        <h1>HD</h1>
+      </header>
 
-  <div className="container">
-    <div className="left-side">
-      <div className="signup-form">
-        <h2>Sign Up</h2>
-        <p className="subtitle">Sign up to enjoy the feature of HD</p>
+      <div className="container">
+        <div className="left-side">
+          <div className="signup-form">
+            <h2>Sign Up</h2>
+            <p className="subtitle">Sign up to enjoy the feature of HD</p>
 
-        <form>
-          <div className="input-group">
-            <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" placeholder="Name" name="name" required value={formData.name} onChange={handleChange}/>
-          </div>
-
-          <div className="input-group">
-            <label htmlFor="date">Date of Birth</label>
-            <input type="date" id="date" placeholder="Date of Birth" name="dateOfBirth" required value={formData.dateOfBirth} onChange={handleChange}/>
-          </div>
-
-          <div className="input-group">
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder="Email" name="email" required value={formData.email} onChange={handleChange}/>
-          </div>
-          {(step === 1)?(
-            <button type="button" className="btn blue" onClick={sendOtp}>Get OTP</button>
-          ):(
-            <>
+            <form>
               <div className="input-group">
-                <input type="password" id="otp" placeholder="Enter OTP" name="otp" required value={formData.otp} onChange={handleChange}/>
+                <label htmlFor="name">Your Name</label>
+                <input
+                  type="text"
+                  id="name"
+                  placeholder="Name"
+                  name="name"
+                  required
+                  value={formData.name}
+                  onChange={handleChange}
+                />
               </div>
-    
-              <button type="submit" className="btn blue" onClick={verifyOtp}>Sign Up</button>
-            </>
-          )};
+              <div className="input-group">
+                <label htmlFor="date">Date of Birth</label>
+                <input
+                  type="date"
+                  id="date"
+                  placeholder="Date of Birth"
+                  name="dateOfBirth"
+                  required
+                  value={formData.dateOfBirth}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="input-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="Email"
+                  name="email"
+                  required
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </div>
+              {step === 1 ? (
+                <button type="button" className="btn blue" onClick={sendOtp}>
+                  Get OTP
+                </button>
+              ) : (
+                <>
+                  <div className="input-group">
+                    <input
+                      type="password"
+                      id="otp"
+                      placeholder="Enter OTP"
+                      name="otp"
+                      required
+                      value={formData.otp}
+                      onChange={handleChange}
+                    />
+                  </div>
 
-        </form>
+                  <button
+                    type="submit"
+                    className="btn blue"
+                    onClick={verifyOtp}
+                  >
+                    Sign Up
+                  </button>
+                </>
+              )}
+            </form>
 
-        <p className="signin-text">Already have an account? <p onClick={()=>setShowSignup(false)}>Sign in</p></p>
-        <p>{message}</p>
+            <p className="signin-text">
+              Already have an account?{" "}
+              <span
+                onClick={() => setShowSignup(false)}
+                style={{ cursor: "pointer", color: "blue" }}
+              >
+                Sign in
+              </span>
+            </p>
+            <p>{message}</p>
+          </div>
+        </div>
+
+        <div className="right-side">
+          <img src="/right-column.png" alt="" />
+        </div>
       </div>
     </div>
-
-    <div className="right-side">
-      <img src="/right-column.png" alt="" />
-    </div>
-  </div>
-</div>
-
   );
 }
+
